3f10ac449ed78f22d64ab89634eba1d3291d8436
Martin Eley <meley@salesforce.com>
2020-03-24 16:34:08 +0000

    Updated package file

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+var path = require('path');
+var serveStatic = require('serve-static');
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(bodyParser.json());
+app.use(serveStatic(__dirname + "/client/dist"));
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.json({
         message: 'Hello World!'
     });
